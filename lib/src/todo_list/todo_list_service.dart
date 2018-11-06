166caea90df2189892d606c632b81d8a7de5dd3c
Shagun Bakliwal <shagunbakliwal@gmail.com>
2018-11-06 09:10:49 +0530

    Prepopulated List

--- lib/src/todo_list/todo_list_service.dart
+++ lib/src/todo_list/todo_list_service.dart
@@ -1,11 +1,40 @@
 import 'dart:async';
-
+import 'dart:html';
 import 'package:angular/core.dart';
 
 /// Mock service emulating access to a to-do list stored on a server.
 @Injectable()
 class TodoListService {
-  List<String> mockTodoList = <String>[];
+  //List<String> mockTodoList = <String>[];
+  List<String> mockTodoList = thingsTodo().toList();
 
   Future<List<String>> getTodoList() async => mockTodoList;
+}  
+  Iterable<String> thingsTodo() sync* {
+  var actions = ['Walk', 'Wash', 'Feed'];
+  var pets = ['cats', 'dogs'];
+
+  for (var action in actions) {
+    for (var pet in pets) {
+      if (pet == 'cats' && action != 'Feed') continue;
+      yield '$action the $pet';
+    }
+  }
+
+UListElement todoList;
+
+void addTodoItem(String item) {
+  print(item);
+
+  var listElement = LIElement();
+  listElement.text = item;
+  todoList.children.add(listElement);
 }
+
+
+void main() {
+  todoList = querySelector('#todolist');
+  thingsTodo().forEach(addTodoItem);
+}
+}
+
